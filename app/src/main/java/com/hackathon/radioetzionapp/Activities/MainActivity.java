5640f2ce92b66a1e78768abdcc631b670135bff0
bshara-t <bshara2704@gmail.com>
2019-03-26 12:04:23 +0200

    Navigation bar added working full option

--- app/src/main/java/com/hackathon/radioetzionapp/Activities/MainActivity.java
+++ app/src/main/java/com/hackathon/radioetzionapp/Activities/MainActivity.java
@@ -1,16 +1,32 @@
 package com.hackathon.radioetzionapp.Activities;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
+import com.hackathon.radioetzionapp.Fragments.FavoritesFragment;
+import com.hackathon.radioetzionapp.Fragments.HomeFragment;
+import com.hackathon.radioetzionapp.Fragments.CommentsFragment;
 import com.hackathon.radioetzionapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.navbar_layout);
+
+        context = this;
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener((BottomNavigationView.OnNavigationItemSelectedListener) context);
+
+        loadFragment(new HomeFragment());
 
     }
 
@@ -20,6 +36,43 @@ public class MainActivity extends AppCompatActivity {
         // override to do nothing
         // i.e. not to finish the activity
         // so that won't go back to splash screen
+    }
+
+    private boolean loadFragment (Fragment fragment){
+        if (fragment != null) {
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+
+            return true;
+
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        Fragment fragment = null;
+
+        switch (menuItem.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.navigation_dashboard:
+                fragment = new FavoritesFragment();
+                break;
+
+            case R.id.navigation_notifications:
+                fragment = new CommentsFragment();
+                break;
+
+        }
+
+        return loadFragment(fragment);
     }
 }
 
