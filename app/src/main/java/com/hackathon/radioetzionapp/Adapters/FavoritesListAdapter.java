17527782c114fb873107b6f8c28836da79d5958c
iskmz <iskandarmazzawi@gmail.com>
2019-04-06 10:40:18 +0300

    start of favorites ..

---
+++ app/src/main/java/com/hackathon/radioetzionapp/Adapters/FavoritesListAdapter.java
@@ -0,0 +1,46 @@
+package com.hackathon.radioetzionapp.Adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.widget.BaseAdapter;
+
+public class FavoritesListAdapter extends BaseAdapter implements Animation.AnimationListener {
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+}
